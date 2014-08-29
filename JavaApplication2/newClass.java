c0d555c5706b9655edc49716b214ed1489ad8473
bus-5170 <bus-5170@vm5170-13.business.utah.edu>
2014-08-28 19:11:30 -0600

    Testing

---
+++ JavaApplication2/newClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bus-5170
+ */
+public class newClass {
+    public static void main(String[] args){
+        int count = 1;
+                while (count < 101);{
+        System.out.print(count + " ");
+    }
+    }
+}
