85241b7bac5352c17b4e8808d9670fa71707c6ac
bus-5170 <bus-5170@vm5170-14.business.utah.edu>
2014-09-04 20:51:12 -0600

    Playing around with variable i

--- Variables/src/variables/Variables.java
+++ Variables/src/variables/Variables.java
@@ -19,8 +19,22 @@ public class Variables {
         // TODO code application logic here
         int i=0;
         int num = 20;
+        
         System.out.println("The value of variable i is " + i);
+        
+        i =-100;
+        
+        int x=10+num;
+        
+        System.out.println("The value of variable i is " + i);
+        System.out.println("The value of variable x is " + x);
         System.out.println("The other value of variable num is " + num);
+        /*everything you put inside the "" is customizable
+        * the plus operator means when the two sides of a plus side are numbers
+        then the compiler performs an addition
+        * if one of the two side is not a number, but some text then java does not
+        perform an addition
+        */
     }
     
 }
