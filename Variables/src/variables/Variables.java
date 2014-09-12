514911004e324b7e16fd74082105cd9d24f9a1b7
bus-5170 <bus-5170@vm5170-14.business.utah.edu>
2014-09-11 21:01:32 -0600

    performing calculations

--- Variables/src/variables/Variables.java
+++ Variables/src/variables/Variables.java
@@ -52,6 +52,9 @@ public class Variables {
         //when creating a value in decimal you have to put a capital F at the end
         short shortNumber = 43;
         char myChar = 'B';
+        boolean answer = true;
+        boolean answer2 = !true;
+        //answer2 is the opposite of answer
         
         int i2 =0;
         
@@ -61,13 +64,66 @@ public class Variables {
         int quotient = i / num;
         //int quotient2 = num / i;
         //This is a decimal, and INT will drop all the decimal places and contain the whole number
-        float quotient2 = num / i;
+        float quotient2 = ((float)num/i);
+        //so we went from an int to a float, if you do it vice versa you will lose information
         
         System.out.println("Show values " +sum + ", " +difference + ", " +product + ", " +quotient + ", " +quotient2);
+        
+        //short handed way of typing: sum - sum + 10;
+        sum+=10;
+        sum-=10;
+        
         
         System.out.println("Show my " + myFloat);
         System.out.println("Show my " + shortNumber);
         System.out.println("Show my " + myChar);
+        
+        int output = num++;
+        int output2 = ++num;
+        
+        
+        //relational operators
+        
+        int score =90;
+        int attendence =100;
+        if (score > 90){
+            System.out.println("Grade is A");
+            //since 85 is not >90 then it will skip the line above and go to the next string
+        }
+        else if (score >=80 /*&& score<90*/){
+            //&& score<90 <-- this is unnecessary because the code will go to this anyways
+            System.out.println("Grade is a B");
+        }
+        else{
+            System.out.println("not A");
+            System.out.println("Sucker");
+        }//In if else statements, once it encounters one that is true then it will skip the rest
+         //inside if else statements it is a boolean for example: score ==100 is boolean because
+        // it is either yes or no
+        
+        
+        if (score == 100 || attendence == 100){
+            System.out.println("Excellent Work");
+        }
+        /*if (score == 100 && attendence == 100){
+            System.out.println("Terrible work");
+        }*/
+    
+    /**************************************Strings and Arrays ******************/
+        
+    
+        String s = "This is a beaut";
+        String s1 = new String ("Waitttt...");
+        //new String is telling Java to get a object from the class string
+        s = s+ s1;
+        //s1 is a string object
+        System.out.println(s1);
+        
+        int stringLength = s1.length();
+        System.out.println("how long is s1 " +stringLength);
+        
     }
+    
+    
     
 }
