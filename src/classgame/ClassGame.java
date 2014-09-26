2bf12418fab7928c0292a74b6d4911f1c72079f7
bus-5170 <bus-5170@vm5170-14.business.utah.edu>
2014-09-25 20:39:41 -0600

    Initial Import for ClassGame.java

---
+++ src/classgame/ClassGame.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package classgame;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author bus-5170
+ */
+public class ClassGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException 
+            //throws IOException, InputMismatchException 
+    {
+        // TODO code application logic here
+        Scanner console = new Scanner(System.in);
+        int number, error = 0, result, stop;
+        String name = "", reply ="";
+        String titles = null;
+        //only objects can be null, not primitives
+        //null means create the variable, but don't let it point to any address
+        
+        StringBuilder stringToSave = new StringBuilder("");
+        
+      
+        
+        /*System.out.println("Please enter the score that you would like to store (-1 to stop)");
+        
+        number = console.nextInt();
+        if (number!=-1){
+        
+        System.out.println("You entered " +number);
+            System.out.println("Please enter name: ");
+            console.nextLine();
+            name = console.nextLine();
+            System.out.println("Your name is " +name);
+            
+        
+        }
+        //whenever you read some text after some number you need to do two .nextLine()
+        else
+            System.out.println("Goodbye");*/
+        
+        //this bottom part is to catch errors and keep the game going instead of failing
+        do {
+        System.out.println("Please enter the score that you would like to store (-1 to stop)");
+        
+        try{
+            error=0;
+            number = console.nextInt();
+        if (number!=-1){
+        
+        System.out.println("You entered " +number);
+            System.out.println("Please enter name: ");
+            console.nextLine();
+            name = console.nextLine();
+            System.out.println("Your name is " +name);
+            
+        
+        }
+        //whenever you read some text after some number you need to do two .nextLine()
+        else
+            System.out.println("Goodbye");
+        }catch(InputMismatchException ime) {
+            error = -1;
+            System.out.println("You Did not enter a number " +ime);
+            console.nextLine();
+        }
+        }while(error==-1);
+        
+        BufferedWriter outputBuff = new BufferedWriter(
+                                        new FileWriter("outfile.txt", true));
+        
+        
+    }
+    
+}
